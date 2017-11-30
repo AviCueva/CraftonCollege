75ca7ead558522b8cb84a29e8223e98bd8440f8d
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-11-29 21:14:25 -0800

    add encounter class

--- CompSci-110/Project4.cpp
+++ CompSci-110/Project4.cpp
@@ -13,18 +13,19 @@
 using namespace std;
 
 class GameCharacter {
-public:                                           // The class' public functions
-   string   GetName();                            // Name of character
-   int   GetLevel();                              // Level of character
-   int   GetCurrentHealth();                      // Current health of character
-   int   GetAttack();                             // Attack of character
-   int   GetDefense();                            // Defense of character
-   int   GetEvasion();                            // Evasion of character
-   void  SubtractHP(int damageTaken);             // Subtract health from the character's HP
-   void  AddHP(int hpRestored);                   // Add health to the character's HP
+public:                                // The class' public functions
+   string   GetName();                 // Name of character
+   int   GetLevel();                   // Level of character
+   int   GetCurrentHealth();           // Attack of character
+   int   GetAttack();                  // Attack of character
+   int   GetDefense();                 // Attack of character
+   int   GetEvasion();                 // Attack of character
+   void  SubtractHP(int damageTaken);  // Subtract health from the character's HP
+   void  AddHP(int hpRestored);        // Add health to the character's HP
    int   CalculateDamage(int defense);            // Calculate damage dealth usering GetAttack()
-   void  AttackOpponent(GameCharacter& opponent); // Deal damage to a GameCharacter class
+   void  AttackOpponent(GameCharacter& opponent);
    GameCharacter(string initName);
+   GameCharacter();
 private:  // The class' private internal data members
    string    name;
    int       level;
@@ -35,10 +36,22 @@ private:  // The class' private internal data members
    int       evasion;
 };
 
+GameCharacter::GameCharacter() {
+   name  = "Default";             // Default name
+   maxHealth = 100;               // Default price
+   currentHealth = maxHealth;     // Default price
+   attack = 5;
+   defense = 3;
+   evasion = 1;
+   
+   return;
+}
+
+
 GameCharacter::GameCharacter(string initName) {  // Default constructor
-   name  = initName;
-   maxHealth = 100;
-   currentHealth = maxHealth;
+   name  = initName;              // Default name
+   maxHealth = 100;               // Default price
+   currentHealth = maxHealth;     // Default price
    attack = 5;
    defense = 3;
    evasion = 1;
@@ -75,18 +88,72 @@ void GameCharacter::AttackOpponent(GameCharacter& opponent) {
    cout << opponent.GetName() << " took a whopping " << damageTaken << " damage!" << endl;
 }
 
+class Encounter {
+public:
+   GameCharacter getPlayer();
+   GameCharacter getOpponent();
+   void Battle();
+   bool PrintMenu(bool &endEncounter);
+   Encounter(GameCharacter &newPlayer);
+   Encounter(GameCharacter &newPlayer, GameCharacter &newOpponent);
+   Encounter();
+private:
+   GameCharacter player;
+   GameCharacter opponent;
+};
+
+Encounter::Encounter(GameCharacter &newPlayer, GameCharacter &newOpponent) {
+   cout << "Custom constructor called";
+   player = newPlayer;
+   opponent = newOpponent;
+}
+
+bool Encounter::PrintMenu(bool &endEncounter) {
+   int userMenuChoice = 0;
+   cout << getPlayer().GetName() << " has " << getPlayer().GetCurrentHealth() << " health" << endl;
+   cout << getOpponent().GetName() << " has " << getOpponent().GetCurrentHealth() << " health" << endl;
+   cout << endl << endl;
+   
+   cout << "Enter 1 to attack" << endl;
+   cout << "Enter 2 to flee" << endl;
+   cout << "Your choice: ";
+   cin >> userMenuChoice;
+      
+   switch(userMenuChoice) {
+      case 1:
+         getPlayer().AttackOpponent(opponent);
+         break;
+      case 2:
+         endEncounter = true;
+         break;
+      default:
+         cout << "Invalid input" << endl;
+         break;
+      }
+   getOpponent().AttackOpponent(player);
+   return endEncounter;
+}
+
+void Encounter::Battle() {
+   bool endEncounter = false;
+   do {
+     PrintMenu(endEncounter);
+   }while(!endEncounter && (getPlayer().GetCurrentHealth() > 0 && getOpponent().GetCurrentHealth() > 0));
+}
+
+GameCharacter Encounter::getPlayer() {
+   return player;
+}
+
+GameCharacter Encounter::getOpponent() {
+   return opponent;
+}
+
 int main() {
    GameCharacter player("Kyle");
    GameCharacter opponent("Jason");
-   do {
-      cout << endl << endl;
-      cout << "player has " << player.GetCurrentHealth() << " health" << endl;
-      cout << "opponent has " << opponent.GetCurrentHealth() << " health" << endl;
-      player.AttackOpponent(opponent);
-      cout << "after a vicious attack from player, opponent has " << opponent.GetCurrentHealth() << " health." << endl;
-      opponent.AttackOpponent(player);
-      cout << "after a vicious attack from opponent, player has " << player.GetCurrentHealth() << " health." << endl;
-   }while(player.GetCurrentHealth() > 0 && opponent.GetCurrentHealth() > 0);
+   Encounter encounter(player, opponent);
    
+   encounter.Battle();
    return 0;
 }
